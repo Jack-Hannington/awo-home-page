a141939785de1df3e4037d5e3a9ab50c963fa2c7
jack hannington <hannington94@gmail.com>
2023-01-11 12:22:51 +0000

    removed reviews slider

--- utils.js
+++ utils.js
@@ -25,83 +25,77 @@ closeAwoModals.forEach((button) => {
 
 
 
-// document.addEventListener('click', (event) => {
-//   if (!event.target.closest('.awo-hero-modal') && !event.target.classList.contains('close-modal') && showModal) {
-//     showModal.classList.remove('show-modal');
-//   }
-// });
-
 
 
 
 
 // Slider
-const allSlides = document.querySelectorAll(".awo-review-slide");
+// const allSlides = document.querySelectorAll(".awo-review-slide");
 
-let currentSlide = 0;
-let maxValue = allSlides.length;
+// let currentSlide = 0;
+// let maxValue = allSlides.length;
 
-const next = document.querySelector("#next");
-const prev = document.querySelector("#prev");
-
-
-if (currentSlide == 0){
-  prev.style.display = "none";
-}
-
-next.addEventListener("click", () => {
-  if (currentSlide < maxValue - 1) {
-    allSlides[currentSlide].classList.remove("current");
-    currentSlide++;
-    allSlides[currentSlide].classList.add('current');
-    console.log(currentSlide);
-    prev.style.display = "block";
-  }
-  if (currentSlide + 1 == maxValue){
-    next.style.display = "none";
-  }
-});
-
-prev.addEventListener("click", () => {
-  if (currentSlide >= 0) {
-    allSlides[currentSlide].classList.remove("current");
-    currentSlide--;
-    console.log(currentSlide);
-    allSlides[currentSlide].classList.add('current');
-  }
-  if (currentSlide == 0){
-    prev.style.display = "none";
-}
-
-  if (currentSlide < maxValue){
-    next.style.display = "block";
-}
-});
+// const next = document.querySelector("#next");
+// const prev = document.querySelector("#prev");
 
 
-// Touch support
+// if (currentSlide == 0){
+//   prev.style.display = "none";
+// }
 
-let startX;
-let endX;
+// next.addEventListener("click", () => {
+//   if (currentSlide < maxValue - 1) {
+//     allSlides[currentSlide].classList.remove("current");
+//     currentSlide++;
+//     allSlides[currentSlide].classList.add('current');
+//     console.log(currentSlide);
+//     prev.style.display = "block";
+//   }
+//   if (currentSlide + 1 == maxValue){
+//     next.style.display = "none";
+//   }
+// });
 
-allSlides.forEach((slide) => {
-  slide.addEventListener("touchstart", (event) => {
-    startX = event.touches[0].clientX;
-  });
+// prev.addEventListener("click", () => {
+//   if (currentSlide >= 0) {
+//     allSlides[currentSlide].classList.remove("current");
+//     currentSlide--;
+//     console.log(currentSlide);
+//     allSlides[currentSlide].classList.add('current');
+//   }
+//   if (currentSlide == 0){
+//     prev.style.display = "none";
+// }
 
-  slide.addEventListener("touchmove", (event) => {
-    endX = event.touches[0].clientX;
-  });
+//   if (currentSlide < maxValue){
+//     next.style.display = "block";
+// }
+// });
 
-  slide.addEventListener("touchend", (event) => {
-    const threshold = 50;
-    const swipeDistance = endX - startX;
 
-    if (swipeDistance > threshold) {
-      prev.click();
-    } else if (swipeDistance < -threshold) {
-      next.click();
-    }
-  });
-});
+// // Touch support
+
+// let startX;
+// let endX;
+
+// allSlides.forEach((slide) => {
+//   slide.addEventListener("touchstart", (event) => {
+//     startX = event.touches[0].clientX;
+//   });
+
+//   slide.addEventListener("touchmove", (event) => {
+//     endX = event.touches[0].clientX;
+//   });
+
+//   slide.addEventListener("touchend", (event) => {
+//     const threshold = 50;
+//     const swipeDistance = endX - startX;
+
+//     if (swipeDistance > threshold) {
+//       prev.click();
+//     } else if (swipeDistance < -threshold) {
+//       next.click();
+//     }
+//   });
+// });
 
